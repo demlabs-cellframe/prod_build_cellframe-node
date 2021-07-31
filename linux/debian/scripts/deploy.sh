4dd7d73c4122d7dddac0f5dd4c128d7044c95786
aleksandr.vodianov <aleksandr.vodianov@demlabs.net>
2021-07-31 18:59:38 +0300

    fixed port flag

--- linux/debian/scripts/deploy.sh
+++ linux/debian/scripts/deploy.sh
@@ -36,7 +36,7 @@ for pkgfile in $PKGFILES; do
 		scp -r -i $CELLFRAME_REPO_KEY ../prod_build/general/essentials/$pkgname_weblink "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/"
 		if [[ $CI_COMMIT_REF_NAME == "master" ]]; then
 set -x
-			scp -p $REPO_PORT -i $CELLFRAME_REPO_KEY $wd/$PACKAGE_PATH/$pkgname$MOD.deb "$CELLFRAME_REPO_CREDS:~/aptly/repo_update/$pkgname_public$MOD.deb"
+			scp -P $REPO_PORT -i $CELLFRAME_REPO_KEY $wd/$PACKAGE_PATH/$pkgname$MOD.deb "$CELLFRAME_REPO_CREDS:~/aptly/repo_update/$pkgname_public$MOD.deb"
 			ssh -p $REPO_PORT -i $CELLFRAME_REPO_KEY "$CELLFRAME_REPO_CREDS" -- "~/aptly/repo_update.sh"
 set +x
 		fi
