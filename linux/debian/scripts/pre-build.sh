0da72b3842b0a384a5af6930faa5c03f02cb0d9d
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-22 16:37:34 +0300

    upd

--- linux/debian/scripts/pre-build.sh
+++ linux/debian/scripts/pre-build.sh
@@ -126,7 +126,7 @@ errcode=0
 for distr in $HOST_DISTR_VERSIONS; do #we need to install required dependencies under schroot.
 	for arch in $HOST_ARCH_VERSIONS; do
 		echo "$CHROOT_PREFIX-$distr-$arch"
-		schroot -c $CHROOT_PREFIX-$distr-$arch -- sudo prod_build/linux/debian/scripts/chroot/pre-build.sh "$PKG_DEPS" || errcode=$?
+		schroot -c $CHROOT_PREFIX-$distr-$arch -- prod_build/linux/debian/scripts/chroot/pre-build.sh "$PKG_DEPS" || errcode=$?
 		[[ $errcode != 0 ]] && echo "Problems with $CHROOT_PREFIX-$distr-$arch occured. You had installed it, right?"
 	done
 done
