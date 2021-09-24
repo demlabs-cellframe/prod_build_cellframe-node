963495f1c6fd51e428024bfb498b922ddcff0a00
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-24 23:59:38 +0300

    add mini error handler

--- windows/scripts/compile.sh
+++ windows/scripts/compile.sh
@@ -1,5 +1,5 @@
 DESTDIR=$1
 wd=$2
 
-cd $wd && mkdir build && cd build
-x86_64-w64-mingw32.static-cmake .. && make && make install DESTDIR=$DESTDIR
+cd $wd && mkdir build && cd build && \
+x86_64-w64-mingw32.static-cmake .. && make && make install DESTDIR=$DESTDIR || echo "error $?" && exit $?
