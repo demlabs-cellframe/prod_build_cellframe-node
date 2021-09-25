758d51d401aec9ddb77c46e9801a26ca87e37043
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 13:33:46 +0300

    fixed changing in cmake

--- windows/scripts/compile.sh
+++ windows/scripts/compile.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 DESTDIR=$1
 wd=$2
 
+echo "[INF] Building Cellframe-node"
 cd $wd 
 
 . prod_build/general/pre-build.sh
 
-export_variables "prod_build/windows/conf/*"
+export_variables "./prod_build/windows/conf/*"
 
 IFS=" "
 for lib in $LIBS; do
-	new_lib=${lib,,}
+	new_lib=$(echo "$lib" | tr '[:upper:]' '[:lower:]')
+	echo "changing $lib to $new_lib"
 	sed -i "s/$lib/$new_lib/g" CMakeLists.txt
 	sed -i "s/$lib/$new_lib/g" cellframe-sdk/CMakeLists.txt
 	sed -i "s/$lib/$new_lib/g" cellframe-sdk/dap-sdk/net/server/enc_server/CMakeLists.txt
@@ -18,6 +22,7 @@ for lib in $LIBS; do
 	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/dap-sdk/net/server/enc_server/CMakeLists.txt
 	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/dap-sdk/net/server/http_server/CMakeLists.txt
 done
+
 
 
 mkdir build && cd build && \
