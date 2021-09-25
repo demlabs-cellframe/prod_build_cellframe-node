114b1d238aa55fab641cf89bc2e8df2a1a23f307
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 12:18:49 +0300

    add logs

--- windows/scripts/compile.sh
+++ windows/scripts/compile.sh
@@ -1,5 +1,7 @@
 DESTDIR=$1
 wd=$2
+export PATH=$WINDOWS_TOOLCHAIN_PATH/usr/bin:$PATH
 
 cd $wd && mkdir build && cd build && \
-x86_64-w64-mingw32.static-cmake .. && make && make install DESTDIR=$DESTDIR || echo "error $?" && exit $?
+x86_64-w64-mingw32.static-cmake .. && make -j$(nproc) || echo "$PATH error $?" && exit $?
+
