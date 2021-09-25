3f673f2e4f129b7560b52a09beca18215a46e46c
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 16:58:23 +0300

    add cleaning

--- windows/scripts/compile.sh
+++ windows/scripts/compile.sh
@@ -10,7 +10,6 @@ cd $wd
 
 export_variables "./prod_build/windows/conf/*"
 
-
 IFS=" "
 for lib in $LIBS; do
 	new_lib=$(echo "$lib" | tr '[:upper:]' '[:lower:]')
@@ -24,9 +23,7 @@ for lib in $LIBS; do
 	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/dap-sdk/net/server/http_server/CMakeLists.txt
 done
 
-
-
 mkdir build && cd build && \
 x86_64-w64-mingw32.static-cmake .. && make -j$(nproc) && \
-cp -f cellframe-node*.exe $DESTDIR || echo "$PATH error $?" && exit $?
+mv cellframe-node*.exe $DESTDIR  && make distclean && cd .. && rm build || echo "error $?" && exit $?
 
