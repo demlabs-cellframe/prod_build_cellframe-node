867d853ad9a62f2865a6e6d20e46e3cac775f851
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-09-25 12:50:30 +0300

    add changing in cmakes for build into mxe

--- windows/scripts/compile.sh
+++ windows/scripts/compile.sh
@@ -1,7 +1,25 @@
 DESTDIR=$1
 wd=$2
-export PATH=$WINDOWS_TOOLCHAIN_PATH/usr/bin:$PATH
 
-cd $wd && mkdir build && cd build && \
+cd $wd 
+
+. prod_build/general/pre-build.sh
+
+export_variables "prod_build/windows/conf/*"
+
+IFS=" "
+for lib in $LIBS; do
+	new_lib=${lib,,}
+	sed -i "s/$lib/$new_lib/g" CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" cellframe-sdk/CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" cellframe-sdk/dap-sdk/net/server/enc_server/CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" cellframe-sdk/dap-sdk/net/server/http_server/CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/dap-sdk/net/server/enc_server/CMakeLists.txt
+	sed -i "s/$lib/$new_lib/g" python-cellframe/cellframe-sdk/dap-sdk/net/server/http_server/CMakeLists.txt
+done
+
+
+mkdir build && cd build && \
 x86_64-w64-mingw32.static-cmake .. && make -j$(nproc) || echo "$PATH error $?" && exit $?
 
