d56fb2cb02a176edb52b72d931360f45a2f8e1f3
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-05 13:49:16 +0300

    add python paths

--- mac/scripts/compile.sh
+++ mac/scripts/compile.sh
@@ -6,4 +6,8 @@ echo "[INF] Building cellframe-node"
 cd $wd 
 
 mkdir build
-cd build && "$CROSS_COMPILE"cmake  -D CMAKE_C_COMPILER="$CROSS_COMPILE"gcc .. && make -j$(nproc) || exit $?
+cd build && "$CROSS_COMPILE"cmake .. \
+ -DCMAKE_C_COMPILER="$CROSS_COMPILE"gcc \
+ -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
+ -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))") \
+&& make -j$(nproc) || exit $?
