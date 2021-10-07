ba4f1f0f804ed3b6bdb430d508736f7045f7396d
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-07 14:15:22 +0300

    add cross_build env for mac

--- mac/scripts/compile.sh
+++ mac/scripts/compile.sh
@@ -5,12 +5,9 @@ wd=$1
 echo "[INF] Building cellframe-node"
 cd $wd 
 
-export _CMAKE_OSX_SYSROOT_PATH="MacOS"
-
 mkdir build
 cd build && 
- "$CROSS_COMPILE"cmake .. \
+ "$CROSS_COMPILE"cmake .. -DMACOS_ARCH="x86_64" \
 && make -j$(nproc) || exit $?
 
-unset _CMAKE_OSX_SYSROOT_PATH 
 exit 0
