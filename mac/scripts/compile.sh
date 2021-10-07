f4a3796d0656af0693919b5e05262b5be5f7d398
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-10-07 13:47:46 +0300

    add mac env

--- mac/scripts/compile.sh
+++ mac/scripts/compile.sh
@@ -5,7 +5,12 @@ wd=$1
 echo "[INF] Building cellframe-node"
 cd $wd 
 
+export _CMAKE_OSX_SYSROOT_PATH="MacOS"
+
 mkdir build
 cd build && 
  "$CROSS_COMPILE"cmake .. \
 && make -j$(nproc) || exit $?
+
+unset _CMAKE_OSX_SYSROOT_PATH 
+exit 0
