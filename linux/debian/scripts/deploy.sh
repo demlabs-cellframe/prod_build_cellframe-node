511c69006963f0815445a09d3e8631b5f494c189
aleksandr.vodianov <aleksandr.vodianov@demlabs.net>
2021-11-03 15:28:41 +0300

    fixed mistakes

--- linux/debian/scripts/deploy.sh
+++ linux/debian/scripts/deploy.sh
@@ -33,7 +33,7 @@ for pkgfile in $PKGFILES; do
 		echo "REF_NAME is $CI_COMMIT_REF_NAME"
 		ssh -i $CELLFRAME_REPO_KEY "$CELLFRAME_FILESERVER_CREDS" "mkdir -p $CELLFRAME_FILESERVER_PATH/$SUBDIR"
 		scp -i $CELLFRAME_REPO_KEY $wd/$PACKAGE_PATH/$pkgname$MOD.deb "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/$pkgname_public$MOD.deb"
-		if [[ $(echo $pkgname | grep "pgsql\|dbg") == "" ]]
+		if [[ $(echo $pkgname | grep "pgsql\|dbg") == "" ]]; then
 			scp -r -i $CELLFRAME_REPO_KEY ../prod_build/general/essentials/$pkgname_weblink "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/"
 		fi
 		if [[ $CI_COMMIT_REF_NAME == "master" ]]; then
