6af210b8bd4d54bf688afded1171baa2cc2d1500
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2021-11-22 10:40:27 +0000

    Support 5095

--- general/update_dashboard.sh
+++ general/update_dashboard.sh
@@ -3,8 +3,9 @@
 git clone https://${USER}:${CI_PUSH_TOKEN}@gitlab.demlabs.net/cellframe/cellframe-dashboard.git
 cd cellframe-dashboard
 git remote set-url origin https://${USER}:${CI_PUSH_TOKEN}@gitlab.demlabs.net/cellframe/cellframe-dashboard.git
-
 git checkout master
+git submodule init
+git submodule update
 
 versionPatch=$(cat config.pri | grep 'VER_PAT =' | cut -d'=' -f 2)
 echo "version patch = $versionPatch"
@@ -14,7 +15,7 @@ sed -i "s/VER_PAT = [0-9]\+/VER_PAT = $versionPatch/g" config.pri
 
 #update cellframe-node
 
-cd cellframe-node && git checkout master && cd -
+cd cellframe-node && git checkout master && git pull && cd -
 
 git add config.pri cellframe-node
 git commit -m 'update version patch'
