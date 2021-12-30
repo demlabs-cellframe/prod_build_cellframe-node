de7368cbca616939e9b36329a81d75b8a15a54bb
aleksandr.vodianov@demlabs.net <aleksandr.vodianov@demlabs.net>
2021-12-30 23:15:33 +0300

    fixed deploy

--- linux/debian/scripts/compile_and_pack.sh
+++ linux/debian/scripts/compile_and_pack.sh
@@ -48,7 +48,7 @@ substitute_pkgname_postfix && mkdir -p build && cd build && ${CMAKE_PATH}cmake .
 && ${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=Debug ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
 
 echo $error
-if [[ $(echo $DISTR_CODENAME| grep bullseye) != "" && $error == 0 ]]; then
+if [[ $(echo $DISTR_CODENAME | grep bullseye) != "" && $error == 0 ]]; then
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_ARM64
 	${CMAKE_PATH}cmake -DCMAKE_C_COMPILER=$ARM64_C_COMPILER -DCMAKE_CXX_COMPLIER=$ARM64_CXX_COMPILER -DCMAKE_TARGET_ARCH="arm64" .. && make -j$(nproc) && \
 	${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * && \
@@ -60,7 +60,7 @@ if [[ $(echo $DISTR_CODENAME| grep bullseye) != "" && $error == 0 ]]; then
 	unset LD_LIBRARY_PATH
 fi
 
-if [[ $(echo $BUILD_TYPE | grep buster) != "" && $error == 0 ]]; then
+if [[ $(echo $DISTR_CODENAME | grep buster) != "" && $error == 0 ]]; then
 	sed -ibak 's/#set(BUILD_WITH_GDB_DRIVER_PGSQL ON)/set(BUILD_WITH_GDB_DRIVER_PGSQL ON)/' ../CMakeLists.txt || error=$?
 	${CMAKE_PATH}cmake ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * \
 	&& ${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=Debug ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
