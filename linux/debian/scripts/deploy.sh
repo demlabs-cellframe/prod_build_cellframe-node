441736adec119d37c1d5bc874a1b9afd4645fc84
Aleksandr Vodianov <aleksandr.vodianov@demlabs.net>
2022-02-04 15:41:20 +0000

    Hotfix 5526 1

--- linux/debian/scripts/deploy.sh
+++ linux/debian/scripts/deploy.sh
@@ -4,7 +4,8 @@ echo "Deploying to $PACKAGE_PATH"
 echo $wd
 
 CELLFRAME_REPO_KEY="~/.ssh/demlabs_publish"
-REPO_PORT=34768
+PUB_REPO_PORT=34768
+PVT_REPO_PORT=22
 CELLFRAME_PUB_REPO_CREDS="admin@debian.pub.demlabs.net"
 CELLFRAME_PUB_CREDS="admin@pub.cellframe.net"
 CELLFRAME_PUB_PATH="~/web/pub.cellframe.net/public_html/linux"
@@ -24,13 +25,15 @@ for pkgfile in $PKGFILES; do
 		CELLFRAME_FILESERVER_CREDS=$CELLFRAME_PUB_CREDS
 		CELLFRAME_FILESERVER_PATH=$CELLFRAME_PUB_PATH
 		CELLFRAME_REPO_CREDS=$CELLFRAME_PUB_REPO_CREDS
+		REPO_PORT=$PUB_REPO_PORT
 	else
 		CELLFRAME_FILESERVER_CREDS=$CELLFRAME_PVT_CREDS
 		CELLFRAME_FILESERVER_PATH=$CELLFRAME_PVT_PATH
 		CELLFRAME_REPO_CREDS=$CELLFRAME_PVT_CREDS
+		REPO_PORT=$PVT_REPO_PORT
 	fi
 	pkgname=$(echo $pkgfile | sed 's/.deb$//')
-	pkgname_public=$(echo $pkgname | cut -d '-' -f1-4,7,9-) #cutting away Debian-9.12
+	pkgname_public=$(echo $pkgname | cut -d '-' -f1-4,7,8) #cutting away Debian-9.12
 	pkgname_weblink="$(echo $pkgname | cut -d '-' -f2,8 )-latest" #leaving only necessary entries
 	echo "Package name for public is $pkgname_public"
 	mv $pkgfile $wd/$PACKAGE_PATH/$pkgname.deb || { echo "[ERR] Something went wrong in publishing the package. Now aborting."; exit -4; }
@@ -39,6 +42,7 @@ for pkgfile in $PKGFILES; do
 	sed -i "/document/s/cellframe.*deb/$pkgname_public.deb/" ../prod_build/general/essentials/$pkgname_weblink/index.html
 	echo "REF_NAME is $CI_COMMIT_REF_NAME"
 	ssh -i $CELLFRAME_REPO_KEY "$CELLFRAME_FILESERVER_CREDS" "mkdir -p $CELLFRAME_FILESERVER_PATH/$SUBDIR"
+	echo "added $pkgname.deb how $pkgname_public.deb"
 	scp -i $CELLFRAME_REPO_KEY $wd/$PACKAGE_PATH/$pkgname.deb "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/$pkgname_public.deb"
 	if [[ $CI_COMMIT_REF_NAME == "master" && $(echo $pkgname | grep "dbg") == "" ]]; then
 		scp -r -i $CELLFRAME_REPO_KEY ../prod_build/general/essentials/$pkgname_weblink "$CELLFRAME_FILESERVER_CREDS:$CELLFRAME_FILESERVER_PATH/$SUBDIR/"
