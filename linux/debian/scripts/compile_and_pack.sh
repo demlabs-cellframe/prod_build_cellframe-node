eaa4e838cfb3469250bafcccb272b6f6023a01e1
dpuzyrkov <dpuzyrkov@gmail.com>
2022-08-30 14:50:45 +0300

    [*] debug test

--- linux/debian/scripts/compile_and_pack.sh
+++ linux/debian/scripts/compile_and_pack.sh
@@ -48,6 +48,10 @@ mkdir -p packages
 echo "Build for $ARCH_VERSION architectures"
 substitute_pkgname_postfix && mkdir -p build && cd build
 
+if [[ $CI_COMMIT_REF_NAME =~ ^.*-rwd$ ]]; then
+		echo "==== Will build a rwd packet"
+fi
+
 echo $error
 if [[ $ARCH_VERSION == "arm" ]]; then
 	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_ARM64
@@ -71,16 +75,14 @@ if [[ $ARCH_VERSION == "amd64" ]]; then
 	&& ${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=Debug ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
 	
 
-	if [[ $CI_COMMIT_REF_NAME =~ ^.*-rwd$ ]]; then
-			echo "==== Building with reldebuginfo"
-			${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
-	fi
+	
+	echo "==== Building with reldebuginfo"
+	${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
+
 
 	sed -ibak 's/#set(BUILD_WITH_GDB_DRIVER_PGSQL ON)/set(BUILD_WITH_GDB_DRIVER_PGSQL ON)/' ../CMakeLists.txt || error=$?
 	${CMAKE_PATH}cmake ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * \
 	&& ${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=Debug ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
-
-	
 fi
 
 cd .. && rm -r build
