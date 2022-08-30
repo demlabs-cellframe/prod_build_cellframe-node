e0094e4d881a2e009df87686b8ec234cea44ef88
dpuzyrkov <dpuzyrkov@gmail.com>
2022-08-30 14:44:14 +0300

    [*] oncemore check

--- linux/debian/scripts/compile_and_pack.sh
+++ linux/debian/scripts/compile_and_pack.sh
@@ -71,7 +71,7 @@ if [[ $ARCH_VERSION == "amd64" ]]; then
 	&& ${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=Debug ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
 	
 
-	if [[ "$CI_COMMIT_REF_NAME" =~ ^.*-rwd$ ]]; then
+	if [[ $CI_COMMIT_REF_NAME =~ ^.*-rwd$ ]]; then
 			echo "==== Building with reldebuginfo"
 			${CMAKE_PATH}cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../ && make -j$(nproc) && ${CMAKE_PATH}cpack && repack *.deb && mv -v *.deb ../packages/ && rm -r * || error=$?
 	fi
