f5b940541d3e3520cedfbdfba3b1ece7a854f908
dmitry <dmitry.puzyrkov@demlabs.net>
2023-08-28 16:05:34 +0700

    ...

--- build.sh
+++ build.sh
@@ -21,14 +21,16 @@ MHERE="$DIR"
 
 export SOURCES=${MHERE}/../
 
-case "${UNAME_OUT}" in
+NAME_OUT="$(uname -s)"
+case "${NAME_OUT}" in
     Linux*)     MACHINE=Linux;;
     Darwin*)    MACHINE=Mac;;
     CYGWIN*)    MACHINE=Cygwin;;
     MINGW*)     MACHINE=MinGw;;
     MSYS_NT*)   MACHINE=Git;;
-    *)          MACHINE="UNKNOWN:${UNAME_OUT}"
+    *)          MACHINE="UNKNOWN:${NAME_OUT}"
 esac
+
 
 
 #validate input params
@@ -68,7 +70,25 @@ set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 BUILD_TYPE="${1:-release}"
 BUILD_OPTIONS="${@:2}"
 
-BUILD_TARGET="${TARGET:-linux}"
+
+DEFAULT_TARGET="linux"
+if [ "$MACHINE" == "Mac" ]
+then
+  DEFAULT_TARGET="osx"
+fi
+
+if [ "$MACHINE" == "Linux" ]
+then
+  DEFAULT_TARGET="linux"
+fi
+
+if [ "$MACHINE" == "Git" ]
+then
+  DEFAULT_TARGET="windows"
+fi
+
+echo "Host machin is $MACHINE"
+BUILD_TARGET="${TARGET:-$DEFAULT_TARGET}"
 
 BUILD_DIR=${PWD}/build_${BUILD_TARGET}_${BUILD_TYPE}
 
